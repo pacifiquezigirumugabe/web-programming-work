e51d029a59e989213d110fbbaa894a4e05eb4854
pacifiquezigirumugabe <117974964+pacifiquezigirumugabe@users.noreply.github.com>
2023-03-29 16:35:32 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="page-footer" style="margin-top: 120px; box-shadow: 0px 0px 2px white; background-color: rgb(17,17,17)">
   <div class="row wide-container">
     <div class="col s3">
-      <h4 class="white-text bold underline">OG Tech PC</h4>
+      <h4 class="white-text bold underline"> Tech PC</h4>
       <p class="grey-text text-lighten-4">Your favorite online PC store.</p>
     </div>
     <div class="col s2">
@@ -23,8 +23,8 @@
         Monday to Sunday : <br> 11.00am to 8.00pm
       </p>
       <p class="bold">
-        B-2-16, SD2, Dataran SD PJU9, <br>
-        Jalan Dataran SD 2, Bandar <br> Sri Damansara, 52200 KL
+        kigali-Rwanda <br>
+       
       </p>
     </div>
     <div class="col s2">
@@ -37,12 +37,12 @@
       </a>
     </div>
     <div class="col s3">
-      <h5 class="white-text bold ">Our Partners</h5>
-      <img src="static/images/Partners.png" />
+      <h5 class="white-text bold ">Our Partner</h5>
+      <img src="static/download.jpg" width="100px"height="100px" />
     </div>
   </div>
   <div class="footer-copyright" style="padding-bottom: 20px;">
-    <div class="wide-container underline">© 2023 OG Tech PC All rights reserved.</div>
+    <div class="wide-container underline">© 2023  Tech PC All rights reserved.</div>
   </div>
 
   <script>
